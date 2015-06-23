5ef58295976845cbf1ca87d4da1e881b4534db72
Tim Fletcher <fletcher0516@gmail.com>
2015-06-23 20:34:43 +0000

    Added Factory girl

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
  test "unique_tag" do
- 	quote = Quote.create(:author => "Tim Fletcher", :saying => "Working with tech is like living sci-fi." )
+ 	quote = FactoryGirl.create(:quote, :author => "Tim Fletcher")
  	expected = "TF#" + quote.id.to_s
  	actual = quote.unique_tag
 
